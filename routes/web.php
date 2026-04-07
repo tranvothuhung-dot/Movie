74cb711c783c494d640daf384dc12aad53d28f65
Thu Hung <030239230065@st.buh.edu.vn>
2026-04-07 09:37:57 +0700

    Yc 4

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
+Route::get('/', [App\Http\Controllers\MovieController4::class, 'index']);
+
+use App\Http\Controllers\MovieController4;
+
+Route::get('/movie/create', [App\Http\Controllers\MovieController4::class, 'create'])->name('movie.create');
+
+Route::post('/movie/store', [App\Http\Controllers\MovieController4::class, 'store']);
+
